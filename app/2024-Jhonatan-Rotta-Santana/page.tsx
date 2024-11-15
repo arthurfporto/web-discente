40305a1ce7d62fd79c51599badba1d376b634fde
Jhonatan Rotta Santana <jhonatan.stn03@hotmail.com>
2024-11-15 14:20:52 -0300

    Create page.tsx

---
+++ app/2024-Jhonatan-Rotta-Santana/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+    return (
+      <div>
+        Link para o meu GitHub &#128073;
+        <a href="https://github.com/nmycry">
+          Jhonatan Rotta Santana
+        </a>
+      </div>
+    );
+  }
+  
